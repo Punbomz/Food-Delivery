17e159c5edb47c0bcbae50898d4b430411fb0578
Punbomz <bombbomb925@gmail.com>
2025-12-15 16:08:39 +0700

    add page to shop login register

--- app/page.tsx
+++ app/page.tsx
@@ -2,23 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-      <div className="bg-red-500 text-white p-4">
-  Tailwind works
-</div>
-
-<button className="btn btn-soft btn-error">
-  DaisyUI works
-</button>
-
-<div className="p-10">
-      <button className="btn btn-primary">
-        Hello daisyUI
+    <div className="p-10 bg-blue-500 text-white">
+      Tailwind test
+      <br />
+      <button className="btn btn-primary mt-4">
+        daisyUI test
       </button>
     </div>
-
-      </main>
-    </div>
-  );
+  )
 }
+
