295f9a2c1d4e8c71e69e5d8e6820baee54b6f14d
Punbomz <bombbomb925@gmail.com>
2025-12-17 04:15:00 +0700

    add shop sidebar icons

--- app/shop/components/sidebar.tsx
+++ app/shop/components/sidebar.tsx
@@ -7,11 +7,11 @@ export function Sidebar() {
     return(
         <>
             <div className="flex-1/4 hidden lg:flex shadow-lg rounded-box p-10 text-center bg-white">
-                <ul className="space-y-10">
-                <li className={`rounded-box btn btn-ghost btn-block btn-lg w-full ${pathname === "/shop/order" ? "btn-active text-white btn-success" : "" }`}><Link href="/shop/order">คำสั่งซื้อ</Link></li>
-                <li className={`rounded-box btn btn-ghost btn-block btn-lg w-full ${pathname === "/shop/menu" ? "btn-active text-white btn-success" : "" }`}><Link href="/shop/menu">รายการอาหาร</Link></li>
-                <li className={`rounded-box btn btn-ghost btn-block btn-lg w-full ${pathname === "/shop" ? "btn-active text-white btn-success" : "" }`}><Link href="/shop">โปรไฟล์</Link></li>
-                <li className={`rounded-box btn btn-ghost btn-block btn-lg w-full ${pathname === "/shop/report" ? "btn-active text-white btn-success" : "" }`}><Link href="/shop/report">ยอดขาย</Link></li>
+                <ul className="space-y-10 w-full">
+                <li><Link href="/shop/order" className={`rounded-box btn btn-ghost btn-block btn-lg w-full ${pathname === "/shop/order" ? "btn-active text-white btn-success" : "" }`}>คำสั่งซื้อ</Link></li>
+                <li><Link href="/shop/menu" className={`rounded-box btn btn-ghost btn-block btn-lg w-full ${pathname === "/shop/menu" ? "btn-active text-white btn-success" : "" }`}>รายการอาหาร</Link></li>
+                <li><Link href="/shop" className={`rounded-box btn btn-ghost btn-block btn-lg w-full ${pathname === "/shop" ? "btn-active text-white btn-success" : "" }`}>โปรไฟล์</Link></li>
+                <li><Link href="/shop/report" className={`rounded-box btn btn-ghost btn-block btn-lg w-full ${pathname === "/shop/report" ? "btn-active text-white btn-success" : "" }`}>ยอดขาย</Link></li>
                 </ul>
             </div>
         </>
