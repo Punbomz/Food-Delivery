7be6d44b2016f67a7449bef4d6c09e3b36bf3d89
kmmiewww <Kmmiewww@gmail.com>
2025-12-19 13:14:02 +0700

    ฟdd shop login/logout history UI

--- app/api/history/route.ts
+++ app/api/history/route.ts
@@ -2,9 +2,11 @@ import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const history = await prisma.usageHistory.findMany({
-    orderBy: { checkIn: "desc" },
-    include: { user: true },
+  const history = await prisma.history.findMany({
+    orderBy: { login: "desc" },
+    include: {
+      shop: true,
+    },
   });
 
   return NextResponse.json(history);
